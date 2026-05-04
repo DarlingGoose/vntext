9dc89a8522907270c9e1d0ca1d7b60916fea78ca
DarlingGoose <seannmoser@gmail.com>
2026-05-04 11:57:05 -0700

    fix: add log removal

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,7 +1,10 @@
 package game
 
 import (
+	"os"
 	"time"
+
+	"github.com/DarlingGoose/vntext/pkg/util"
 )
 
 type RunnerType string
@@ -45,6 +48,25 @@ type Game struct {
 	EnvVars         []EnvVar `json:"env_vars"`
 	EngineName      string   `json:"engine_name"`
 }
+
+func (g *Game) DeleteLog() error {
+	if !util.IsFile(g.TextHookLogFile) {
+		return nil
+	}
+	return os.Remove(g.TextHookLogFile)
+}
+func (g *Game) LogSize() (int64, error) {
+	if !util.IsFile(g.TextHookLogFile) {
+		return 0, nil
+	}
+	stat, err := os.Stat(g.TextHookLogFile)
+	if err != nil {
+		return 0, err
+	}
+
+	return stat.Size(), err
+}
+
 type EnvVar struct {
 	Key   string
 	Value string
--- pkg/util/text.go
+++ pkg/util/text.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -85,4 +86,32 @@ func isMostlyASCII(s string) bool {
 
 	// threshold: 80% ASCII letters
 	return float64(asciiCount)/float64(total) > 0.8
+}
+
+func ByteCountIEC(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	// "KMGTPE" represents Kilo, Mega, Giga, Tera, Peta, Exa
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "KMGTPE"[exp])
+}
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
 }
